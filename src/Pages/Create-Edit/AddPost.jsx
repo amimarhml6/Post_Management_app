0bb78075197b02156f14fff8c048703eb982032c
hadjer758 <hadjeroubahi14@gmail.com>
2025-02-14 17:08:53 +0100

    hadjer branch header footer

---
+++ src/Pages/Create-Edit/AddPost.jsx
@@ -0,0 +1,15 @@
+import './AddPost.css'
+import CreatePost from '../../Components/day3Finalproject/CreatePost'
+import Navbar from '../../Components/my-componenets/Navbar'
+
+function Page1() {
+
+  return (
+    <>
+      <Navbar/>
+      <CreatePost />
+    </>
+  )
+}
+
+export default Page1
